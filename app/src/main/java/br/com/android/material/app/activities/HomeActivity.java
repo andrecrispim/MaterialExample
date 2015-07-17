6fb58dc6f2899b923fac7a0ec4c7c4ce3ed0f402
andrecrispim <acrispim@terra.com.br>
2015-07-17 16:32:38 -0300

    chore(library): Incluir telas inicial e principal

--- app/src/main/java/br/com/android/material/app/activities/HomeActivity.java
+++ app/src/main/java/br/com/android/material/app/activities/HomeActivity.java
@@ -2,11 +2,13 @@ package br.com.android.material.app.activities;
 
 import org.androidannotations.annotations.EActivity;
 
+import br.com.android.material.R;
+
 /**
  * Tela principal da aplicação
  *
  * @author andre.moreira
  */
-@EActivity
+@EActivity(R.layout.activity_home)
 public class HomeActivity extends DaggerActivity {
 }
--- app/src/main/java/br/com/android/material/app/activities/SplashScreenActivity.java
+++ app/src/main/java/br/com/android/material/app/activities/SplashScreenActivity.java
@@ -1,12 +1,30 @@
 package br.com.android.material.app.activities;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EActivity;
+
+import br.com.android.material.R;
 
 /**
  * Tela inicial da aplicação
  *
  * @author andre.moreira
  */
-@EActivity
+@EActivity(R.layout.activity_splash_screen)
 public class SplashScreenActivity extends DaggerActivity {
+
+    /**
+     * Inicializa
+     *
+     * Seria utilizado para buscar dados que devem estar disponíveis ao iniciar a
+     * aplicação.
+     */
+    @AfterViews
+    @Background(delay = 2000)
+    protected void initialize() {
+        // Atenção: o delay é apenas par exemplifcar, se não tem nada para busca deveria ir direto
+        // para a Home
+        HomeActivity_.intent(this).start();
+    }
 }
